7b4fd4ea7393e8373fd6a0ccc1eeeabc7fa731ad
zainaazam <zaina.azam@gmail.com>
2021-12-06 16:50:18 +0200

    Main Page responsive Done

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -41,6 +41,25 @@ const Container = styled.div`
   flex-direction: row;
   justify-content: space-evenly;
   padding: 100px 0px 150px;
+  @media (max-width: 800px) {
+    flex-wrap: wrap;
+    width: 60%;
+    margin: auto;
+    justify-content: space-between;
+  }
+  @media (max-width: 700px) {
+    width: 80%;
+  }
+  @media (max-width: 500px) {
+    width: 90%;
+  }
+  @media (max-width: 470px) {
+    width: 40%;
+    padding: 10px 0px 80px;
+  }
+  @media (max-width: 400px) {
+    width: 50%;
+  }
 `;
 
 const Option = styled.div`
@@ -57,13 +76,49 @@ const Option = styled.div`
     background-color: ${Theme.colors.lightBlue};
   }
   transition: all 0.4s;
+  @media (max-width: 1300px) {
+    width: 250px;
+    height: 330px;
+  }
+  @media (max-width: 1100px) {
+    width: 200px;
+    height: 280px;
+  }
+  @media (max-width: 900px) {
+    width: 180px;
+    height: 230px;
+  }
+  @media (max-width: 800px) {
+    margin-top: 40px;
+  }
 `;
 
 const Image = styled.img`
   width: 160px;
   height: 160px;
+  @media (max-width: 1300px) {
+    width: 120px;
+    height: 120px;
+  }
+  @media (max-width: 1100px) {
+    width: 100px;
+    height: 100px;
+  }
+  @media (max-width: 900px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 const Title = styled(CustomText)`
   font-size: 50px;
+  @media (max-width: 1300px) {
+    font-size: 40px;
+  }
+  @media (max-width: 1100px) {
+    font-size: 30px;
+  }
+  @media (max-width: 900px) {
+    font-size: 25px;
+  }
 `;
